d76af40d78d571a2153cf5b89de6fda69cc0ea19
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-02 21:56:28 +0100

    Create boolean.js

---
+++ boolean.js
@@ -0,0 +1,24 @@
+// comparisons 
+let temp = 18 
+const perfect = 18 
+
+// Let's check if th temp is perfect for a day out 
+console.log(temp === perfect) 
+
+// Do we have enough money for a day out? 
+
+let money = 9 
+const travel = 4 
+const food = 5 
+
+console.log (money >= (travel + food))
+
+// numbers and strings below
+console.log ( 5 == "5")
+
+//different comparison operator 
+console.log ( (5*2) === 10)
+
+// we set weather as nice to be true - never use quotiation marks for true or false
+let isWeatherNice = true 
+console.log (isWeatherNice)
