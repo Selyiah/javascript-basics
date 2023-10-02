8f31cae1658867eb4ab61b6baba89734ba17077b
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-02 23:11:11 +0100

    Create example.js

---
+++ updating-CSS-using-JS/example.js
@@ -0,0 +1,4 @@
+document.getElementById("p2").style.color = "blue";
+document.getElementById("p1").style.color = "pink";
+document.getElementById("p2").style.fontFamily = "Arial";
+document.getElementById("p2").style.fontSize = "larger";
