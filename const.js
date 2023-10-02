a98ce1ea2dd8f10ede0a2a21ed4cd07c29230f3b
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-02 21:43:16 +0100

    Create const.js

---
+++ const.js
@@ -0,0 +1,16 @@
+const greeting ="Hello everyone I am Melissa"
+const emoji="♥"
+      
+//interpolation
+const name= `${emoji} ${greeting} ${emoji}`
+
+//concatenation
+const name1 = emoji + " " + greeting + " " + emoji 
+console.log(name)
+
+const message= emoji+ " " + greeting + " "
++ emoji
+console.log(3)
+console.log(message)
+console.log(greeting)
+console.log(emoji)
