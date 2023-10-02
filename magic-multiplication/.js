69cd360fe306c33401a5f35b299d47dfc6138a0e
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-02 23:57:47 +0100

    Create .js

---
+++ magic-multiplication/.js
@@ -0,0 +1,38 @@
+// getting the elements from our HTML
+const calculateButton = document.getElementById('calculateButton');
+        const resultElement = document.getElementById('result');
+
+// adding our event listener
+// the button click triggers our function
+        calculateButton.addEventListener('click', function() {
+            const number1 = document.getElementById('number1').value;
+            const number2 = document.getElementById('number2').value;
+
+          // if / else statement to check whether either of the numbers entered is empty / isn't a number! This is what Nan means. 
+          // else, multiply the two numbers.
+            if (isNaN(number1) || isNaN(number2)) {
+                resultElement.textContent = 'Please enter valid numbers!';
+            } else {
+                let result = number1 * number2;
+                resultElement.textContent = 'Answer: ' + '✨' + result + '✨';
+            }
+        });
+
+let clearButton = document.getElementById("clearResult");
+
+clearButton.addEventListener('click', ()=> {
+  resultElement.remove()
+})
+
+//const clearResult = document.getElementById('clearResult');
+        
+//clearResult.addEventListener('click', function() {
+    //resultElement.textContent = '';
+//   document.getElementById('number1').value='';
+ //  document.getElementById('number2').value='';
+//});
+
+
+
+
+
