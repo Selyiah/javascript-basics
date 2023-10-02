cf8e25e1596bf0b1302aeb6993cb053208f80342
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-02 22:51:45 +0100

    Create style-customiser.js

---
+++ web-app-with-CSS/style-customiser.js
@@ -0,0 +1,13 @@
+function applyBackgroundColor() {
+  const selectedBgColor = document.getElementById("bg-color").value;
+  document.body.style.backgroundColor = selectedBgColor;
+}
+
+function applyFontFamily() {
+  const selectedFont = document.getElementById("font-family").value;
+  document.body.style.fontFamily = selectedFont;
+}
+
+function applyFontColor() {
+  //insert code here
+}
