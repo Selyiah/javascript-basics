690ebc2276d167f425eae7888b7630ba2d5a1686
Sely <140277594+Selyiah@users.noreply.github.com>
2023-10-02 21:47:31 +0100

    Update const.js

--- const.js
+++ const.js
@@ -1,3 +1,4 @@
+// gretting message 
 const greeting ="Hello everyone I am Melissa"
 const emoji="♥"
       
@@ -14,3 +15,44 @@ console.log(3)
 console.log(message)
 console.log(greeting)
 console.log(emoji)
+
+
+// currency converter 
+const gbp2egp = 38.62
+const gbp = prompt("Amount in GBP?") 
+
+const egp = gbp * gbp2egp 
+
+console.log(egp + " EGP")
+
+// prompt rate 
+const gbp = prompt ("Enter an amount in GBP", 10)
+const rate = 1.5 
+const result = gbp * rate 
+
+console.log(`That's ${result} future coins`)
+
+
+
+
+// prompts 
+const greeting ="Welcome to friends world"
+const emoji= "👬 "
+
+//concatenation
+const finalgreeting = " " + greeting + " " + emoji 
+console.log(finalgreeting)
+
+//Please tell me your name
+const name = prompt ("enter your name")
+console.log (`My name is ${name}.`)
+
+//how many friends you got? 
+const yournumber = prompt ("How many friends do you have?", 3)
+const rate = 3 
+const result = yournumber * rate 
+
+//interpolation
+console.log(`I have ${result} friends`)
+//at first i was confused at the alert because I embedded concactenation inside the brackets but after i removed it, it worked fine!
+alert ("Hey! You are popular if you have more than 5 friends")
